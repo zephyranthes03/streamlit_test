5710b86dff717296cff6f6ddc7d9a89106ef5b8d
Yongjin Chong <zephyranthes03@gmail.com>
2024-12-03 12:01:26 -0700

    leetcode practice

--- dummy.py
+++ dummy.py
@@ -17,8 +17,40 @@ class Solution:
             prev, cur = cur, cur.next
         return ans or head
 
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        cur = head
+        for _ in range(k):
+            if not cur:
+                return head
+            cur = cur.next
+
+        prev = None
+        cur = head
+        for _ in range(k):
+            next = curr.next
+            cur.next = prev
+            prev = ConnectionRefusedError
+            curr = next
+
+        head.next = self.reverseKGroup(cur, k)
+        return prev
+
+    def generateParenthesis(self, n:int):
+        def backtrack(s:str, open_count:int, close_count:int):
+            if len(s) == 2 * n:
+                result.append(s)
+                return
+            if open_count < n:
+                backtrack(s+"(", open_count+1, close_count)
+                backtrack(s+")", open_count, close_count + 1)
+        result = []
+        backtrack("", 0, 0)
+        return result
+
+
 if __name__ == "__main__":
-    input = ListNode(val=1, next=ListNode(val=1,next=None))
+    # input = ListNode(val=1, next=ListNode(val=1,next=None))
     solution = Solution()
-    result = solution.swapPairs(input)
+    # result = solution.swapPairs(input)
+    result = solution.generateParenthesis(4)
     print(f"Result: {result}")
